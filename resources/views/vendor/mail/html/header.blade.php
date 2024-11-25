9b2d50b5a757d173a7035e288fde1befc1f76464
Neon <stepan.entsov@gmail.com>
2024-11-25 13:07:16 +0300

    25112024

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,11 +1,14 @@
+@props(['url'])
 <tr>
-<td class="header">
-<a href="{{ $url }}">
-<div class="logo"></div>
-<div>{{ $slot }}</div>
-</a>
-</td>
-</tr>
-<tr>
-<td class="header-bottom"></td>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">
+            <img
+                src="https://fs-thb03.getcourse.ru/fileservice/file/thumbnail/h/13466f741221f7be4c6975413c43c18f.png/s/f1200x/a/755389/sc/5"
+                class="logo"
+                alt="{{ __(config('app.name')) }} Logo">
+            <div>
+            {{ $slot }}
+            </div>
+        </a>
+    </td>
 </tr>
