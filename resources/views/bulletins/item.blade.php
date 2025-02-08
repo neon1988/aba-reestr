12bce911f379e3ba8f23cf5e3928e88ab50fea52
Neon <stepan.entsov@gmail.com>
2025-02-08 11:51:44 +0300

    08022025

--- resources/views/bulletins/item.blade.php
+++ resources/views/bulletins/item.blade.php
@@ -1,11 +1,17 @@
-<div class="w-full mx-auto bg-white dark:bg-gray-800 shadow-lg rounded-lg overflow-hidden mb-4  hover:shadow-xl transition duration-300">
+<div
+    class="w-full mx-auto bg-white dark:bg-gray-800 shadow-lg rounded-lg overflow-hidden mb-4  hover:shadow-xl transition duration-300">
     <div class="p-4 flex items-center">
-        @isset ($item->creator->photo)
-            <x-image :url="optional($item->creator->photo)->url"
-                     :alt="$item->creator->fullName"
-                     width="50" height="50" quality="90"
-                     class="h-12 w-12 rounded-full object-cover border-2 border-gray-200 dark:border-gray-700" />
-        @endisset
+        <a href="{{ route('users.show', ['user' => $item->creator]) }}"
+            class="h-12 w-12 rounded-full bg-gray-300 overflow-hidden flex items-center justify-center shrink-0 text-white font-bold">
+            @if(isset($item->creator->photo))
+                <x-image :url="$item->creator->photo->url"
+                         :alt="$item->creator->fullName"
+                         width="50" height="50" quality="90"
+                         class="w-full h-full object-cover"/>
+            @else
+                <span>{{ $item->creator->nameInitials }}</span>
+            @endif
+        </a>
         <div class="ml-4">
             <a href="{{ route('users.show', ['user' => $item->creator]) }}">
                 <h2 class="text-gray-800 dark:text-white font-semibold">{{ $item->creator->full_name }}</h2>
--- resources/views/components/image.blade.php
+++ resources/views/components/image.blade.php
@@ -1,8 +1,8 @@
-@php use Litlife\Url\Url; @endphp
+@php use Illuminate\Support\Facades\Vite; use Litlife\Url\Url; @endphp
 @props(['url', 'width', 'height', 'quality'])
 
 @php
-    $url = is_string($url) ? $url : 'https://fs-thb03.getcourse.ru/fileservice/file/thumbnail/h/13466f741221f7be4c6975413c43c18f.png/s/f1200x/a/755389/sc/5'; // Замените 'default-url' на нужное значение по умолчанию
+    $url = is_string($url) ? $url : Vite::asset('resources/images/logo_236.png'); // Замените 'default-url' на нужное значение по умолчанию
 @endphp
 
 <img
@@ -12,4 +12,4 @@
          {{ Url::fromString($url)->withQueryParameter('w', $width)->withQueryParameter('h', $height)->withQueryParameter('q', $quality) }} 100w,
          {{ Url::fromString($url)->withQueryParameter('w', $width * 2)->withQueryParameter('h', $height * 2)->withQueryParameter('q', $quality - 15) }} 200w,
          {{ Url::fromString($url)->withQueryParameter('w', $width * 3)->withQueryParameter('h', $height * 3)->withQueryParameter('q', $quality - 30) }} 300w"
-    />
+/>
