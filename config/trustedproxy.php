2efe5d185ca96cede832da4631faef214ed0282a
Neon <stepan.entsov@gmail.com>
2025-03-16 17:45:59 +0300

    16032025

--- config/trustedproxy.php
+++ config/trustedproxy.php
@@ -1,5 +1,14 @@
 <?php
 
+use Illuminate\Http\Request;
+
 return [
-    'proxies' => '*',
+    'proxies' => '*', // Доверять всем прокси (или укажите конкретный IP)
+    'headers' => Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO |
+        Request::HEADER_X_FORWARDED_PREFIX |
+        Request::HEADER_X_FORWARDED_AWS_ELB |
+        Request::HEADER_X_FORWARDED_TRAEFIK
 ];
