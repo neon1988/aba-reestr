196a90f07ccf0a4914556c0de627b04b76a78183
Neon <stepan.entsov@gmail.com>
2025-03-31 17:26:12 +0300

    31032025

--- routes/console.php
+++ routes/console.php
@@ -9,7 +9,7 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command(PaymentUpdateBatch::class, ['minutes' => 10])->hourly();
+Schedule::command(PaymentUpdateBatch::class, [10])->hourly();
 
 Schedule::command('purge:temp "App\Models\File" 1')->daily();
 Schedule::command('purge:soft-deleted "App\Models\File" 30')->daily();
