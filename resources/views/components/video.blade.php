240e95caa13d79f68300bf7184892858a5a01be5
Neon <stepan.entsov@gmail.com>
2025-04-28 11:04:27 +0300

    28042025

--- resources/views/components/video.blade.php
+++ resources/views/components/video.blade.php
@@ -4,3 +4,18 @@
     <source src="{{ $url }}">
     Ваш браузер не поддерживает тег <code>video</code>.
 </video>
+
+<!-- Секция "Видео не воспроизводится?" с Alpine.js -->
+<div class="mt-4" x-data="{ open: false }">
+    <button
+        @click="open = !open"
+        class="w-full text-left text-cyan-600 font-semibold hover:text-cyan-800 transition-colors duration-200 focus:outline-none"
+    >
+        Видео не воспроизводится?
+    </button>
+    <div x-show="open" class="mt-2 text-gray-700">
+        Для корректного просмотра видео рекомендуем использовать современные браузеры: Google Chrome,
+        Яндекс.Браузер или Mozilla Firefox.
+        Это обеспечит лучшее качество воспроизведения и стабильную работу всех функций сайта.
+    </div>
+</div>
--- resources/views/worksheets/show.blade.php
+++ resources/views/worksheets/show.blade.php
@@ -5,21 +5,7 @@
         <div class="flex items-center justify-center">
             <div class="bg-white shadow-lg rounded-lg max-w-4xl w-full p-6">
                 @can('watch', $item)
-                    <x-video :url="route('worksheets.download', ['worksheet' => $item])"/>
-                    <!-- Секция "Видео не воспроизводится?" с Alpine.js -->
-                    <div class="mt-4" x-data="{ open: false }">
-                        <button
-                            @click="open = !open"
-                            class="w-full text-left text-cyan-600 font-semibold hover:text-cyan-800 transition-colors duration-200 focus:outline-none"
-                        >
-                            Видео не воспроизводится?
-                        </button>
-                        <div x-show="open" class="mt-2 text-gray-700">
-                            Для корректного просмотра видео рекомендуем использовать современные браузеры: Google Chrome,
-                            Яндекс.Браузер или Mozilla Firefox.
-                            Это обеспечит лучшее качество воспроизведения и стабильную работу всех функций сайта.
-                        </div>
-                    </div>
+                    <x-video :url="route('worksheets.download', ['worksheet' => $item])" />
                 @else
                     <!-- Webinar Image -->
                     <div class="relative">
