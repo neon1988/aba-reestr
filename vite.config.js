f3659d0b35adadcebbafc9a9638f05e9b16236c8
Neon <stepan.entsov@gmail.com>
2025-06-03 14:57:51 +0300

    03062025

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,11 @@ export default defineConfig({
                 'resources/js/app.js',
                 'resources/images/logo_118.png',
                 'resources/images/logo_236.png',
+                'resources/images/logo_1200.png',
                 'resources/images/IBAO_CEU_Provider.png',
+                'resources/images/materials.jpg',
+                'resources/images/supervisia.jpg',
+                'resources/images/forums.jpg',
             ],
             refresh: true,
         })
